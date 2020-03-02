4bb18415b09b7a1658707ab809a7cf6327989b99
David Clements <magicman86@gmail.com>
2020-03-01 22:40:35 -0800

    Fixed the obvious compile error from the bad merge.

--- src/screens/ManageRecipe.java
+++ src/screens/ManageRecipe.java
@@ -6,8 +6,6 @@ import entities.Recipe;
 import entities.User;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-import util.Helpers;
 import util.Result;
 import util.ui.PaginatedSelect;
 import util.ui.SelectAction;
@@ -23,8 +21,10 @@ public class ManageRecipe {
         menuOptions = new ArrayList<>();
         menuOptions.add("Go back");
         menuOptions.add("Show All Recipes");
+        menuOptions.add("Show Stored Recipes");
+        menuOptions.add("Rate Recipe");
+        menuOptions.add("Add Recipe To Cart");
         menuOptions.add("Delete Recipe");
-        menuOptions.add("Add Recipe");
     }
 
     public static void view(Scanner scanner, ServerDB server, User user) {
@@ -39,18 +39,27 @@ public class ManageRecipe {
                 // process options
                 switch (index) {
                     case (1):
-                        showAllRecipes(scanner, server);
+                        showAllRecipes(scanner, server, user);
                         break;
                     case (2):
                     {
                         //TODO
-                        deleteRecipe(scanner, server);
+                        showStoredRecipes();
                         break;
                     }
                     case (3):
+                        rateRecipe(scanner, server, user);
+                    case (4):
                     {
                         //TODO
-                        addRecipe(scanner,server);
+                        addRecipeCart();
+                        break;
+                    }
+                    case (5):
+                    {
+
+                        //TODO
+                        deleteRecipeCart();
                         break;
                     }
                     default:
@@ -60,13 +69,7 @@ public class ManageRecipe {
         } while (!selected.isBack()); // exit
     }
 
-    public static void showAllRecipes(Scanner scanner, ServerDB server) {
-        /*
-
-            //lists the global recipes, not user dependent
-
-        */
-
+    public static void showAllRecipes(Scanner scanner, ServerDB server, User user) {
         final int increment = 5;
         int start = 0;
         SelectAction<Recipe> action;
@@ -91,7 +94,7 @@ public class ManageRecipe {
                 } else if (action.isPrevious()) {
                     start = Math.max(0, start - 10);
                 } else if (action.isSelected()) {
-                    Helpers.showRecipe(action.getSelected());
+                    showRecipe(action.getSelected());
                 } else { /* isback() handled as exit condition */ }
 
             } else { // system failure
@@ -101,41 +104,98 @@ public class ManageRecipe {
         } while (!action.isBack()); // back button exits the screen
     }
 
-    public static void deleteRecipe(Scanner scanner, ServerDB server){
+    /**
+     * prints individual recipe info. This should be expanded to include an option for adding the recipe
+     * to the user list
+     *
+     * @param recipe
+     */
+    public static void showRecipe(Recipe recipe) {
+        System.out.println("Recipe: " + recipe.getName());
+        System.out.println("\tUrl: " + recipe.getUrl());
+        System.out.println("\tRating: " + recipe.getRating().get());
+    }
+
+    public static void rateRecipe(Scanner scanner, ServerDB server, User user) {
+        SelectAction<Recipe> action;
+        do {
+            //Get records
+            Result<ArrayList<Recipe>> recipesR =
+                RecipeQueries.getRecipes(server, 0, 100);
+
+            if (recipesR.isSuccess()) { // got records
+                ArrayList<Recipe> recipes = recipesR.value();
+                int i =0;
+                for (Recipe recipe : recipes) {
+                    System.out.println(i);
+                    i++;
+                    showRecipe(recipe);
+                }
+                Scanner in = new Scanner(System.in);
+                System.out.println("Which recipe would you like to update the rating for?: ");
+                int recipe = in.nextInt();
+                System.out.println("What would you like to update the rating to?: ");
+                int rating = in.nextInt();
+                Recipe update = recipes.get(recipe);
+                RecipeQueries.updateRecipe(server, update.getRecipeId(), recipes, rating);
+                return;
+            } else { // system failure
+                System.out.println(recipesR.error());
+                return;
+            }
+        } while (!action.isBack());
+    }
+
+    public static void showStoredRecipes(){
         /*
 
-            //TODO modifies the global recipes, not user dependent
+            Lists all the stored recipes that the user has added.
+
+         */
+        System.out.println("TODO Show Stored Recipes");
+        //TODO
+    }
+
+    public static void addRecipeCart(){
+        /*
+
+            Adds a recipe by ID from a list of recipes.
+
+            We should list all the recipes, before asking which recipe they would like to add.
+
+         */
+        System.out.println("TODO Add Recipe To Cart");
+        //TODO
+    }
+
+    public static void deleteRecipeCart(){
+        /*
+
+            Removes a recipe from a user.
 
          */
         System.out.println("TODO Delete Recipe");
         //TODO
     }
 
-    public static void addRecipe(Scanner scanner, ServerDB server) {
-        /*
+    /**public static void addRecipe(Scanner scanner, ServerDB server, User user) {
+     SelectAction<Recipe> action;
+     do {
+     //Get records
+     Result<ArrayList<Recipe>> recipesR =
+     RecipeQueries.getRecipes(server, 0, 100);
 
-            //TODO modifies the global recipes, not user dependent
-
-         */
-        System.out.println("TODO addRecipe");
-
-/*        SelectAction<Recipe> action;
-        do {
-            //Get records
-            Result<ArrayList<Recipe>> recipesR =
-                    RecipeQueries.getRecipes(server, 0, 100);
-
-            if (recipesR.isSuccess()) { // got records
-                ArrayList<Recipe> recipes = recipesR.value();
-                Scanner in = new Scanner(System.in);
-                System.out.println("Which a new recipeID for the recipe: ");
-                String ID = in.nextLine();
-                RecipeQueries.addRecipe(server, ID, recipes);
-                return;
-            } else { // system failure
-                System.out.println(recipesR.error());
-                return;
-            }
-        } while (!action.isBack());*/
-    }
+     if (recipesR.isSuccess()) { // got records
+     ArrayList<Recipe> recipes = recipesR.value();
+     Scanner in = new Scanner(System.in);
+     System.out.println("Which a new recipeID for the recipe: ");
+     String ID = in.nextLine();
+     RecipeQueries.addRecipe(server, ID, recipes);
+     return;
+     } else { // system failure
+     System.out.println(recipesR.error());
+     return;
+     }
+     } while (!action.isBack());
+     }**/
 }
